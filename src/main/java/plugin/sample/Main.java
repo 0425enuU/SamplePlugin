03410a5f8b31a71cbdf695ad9ec19f34421fdd96
enuU <enuu83184@gmail.com>
2024-05-17 20:08:24 +0900

    ローカルのfirst commitです

---
+++ src/main/java/plugin/sample/Main.java
@@ -0,0 +1,111 @@
+package plugin.sample;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
+import org.bukkit.World;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Firework;
+import org.bukkit.entity.Monster;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public final class Main extends JavaPlugin implements Listener {
+
+    private int count;
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+
+    /**
+     * プレイヤーがスニークを開始/終了する際に起動されるイベントハンドラ。
+     *
+     * @param e イベント
+     */
+    @EventHandler
+    public void onPlayerToggleSneak(PlayerToggleSneakEvent e) throws IOException {
+        // イベント発生時のプレイヤーやワールドなどの情報を変数に持つ。
+//        素数で花火を打ち上げ
+//        BigInteger val = new BigInteger(String.valueOf(count));
+//        if(val.isProbablePrime(1)){
+        Player player = e.getPlayer();
+        World world = player.getWorld();
+        List<Color> colorList = List.of(Color.RED, Color.BLUE, Color.WHITE, Color.GREEN);
+        if (count % 2 == 0) {
+            for (Color color : colorList) {
+                // 花火オブジェクトをプレイヤーのロケーション地点に対して出現させる。
+                Firework firework = world.spawn(player.getLocation(), Firework.class);
+
+                // 花火オブジェクトが持つメタ情報を取得。
+                FireworkMeta fireworkMeta = firework.getFireworkMeta();
+
+                // メタ情報に対して設定を追加したり、値の上書きを行う。
+                // 今回は青色で星型の花火を打ち上げる。
+                fireworkMeta.addEffect(
+                    FireworkEffect.builder().withColor(color).with(Type.BURST).withFlicker().build());
+                fireworkMeta.setPower(4);
+
+                // 追加した情報で再設定する。
+                firework.setFireworkMeta(fireworkMeta);
+            }
+            Path path = Path.of("firework.txt");
+            Files.writeString(path, "たーまーやー");
+            player.sendMessage(Files.readString(path));
+        }
+        count++;
+    }
+
+    //    }
+    @EventHandler
+    public void onPlayerBedEnter(PlayerBedEnterEvent e) {
+        Player player = e.getPlayer();
+        ItemStack[] itemStacks = player.getInventory().getContents();
+        Arrays.stream(itemStacks).filter(
+                item -> !Objects.isNull(item) && item.getMaxStackSize() == 1 && item.getAmount() < 64)
+            .forEach(item -> item.setAmount(0));
+
+        player.getInventory().setContents(itemStacks);
+    }
+
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+        player.sendMessage("参加しました！");
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageByEntityEvent e) {
+//     攻撃を受けたエンティティがプレイヤークラスかどうかinstanceofで確認+攻撃を行ったエンティティがモンスタークラスかどうかinstanceofで確認
+        if (e.getEntity() instanceof Player player && e.getDamager() instanceof Monster) {
+//           キャストしてPlayerの参照を取得
+          if (e.getDamager() instanceof Monster || e.getDamager() instanceof Arrow) {
+//           プレイヤーのライフをマックスに設定
+                Objects.requireNonNull(player.getAttribute(Attribute.GENERIC_MAX_HEALTH)).setBaseValue(20.0);
+                player.setHealth(Objects.requireNonNull(player.getAttribute(Attribute.GENERIC_MAX_HEALTH)).getValue());
+            }
+
+        }
+    }
+}
